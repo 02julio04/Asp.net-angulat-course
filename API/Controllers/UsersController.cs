dfec26982b0a3fc6ee30fefa1f81711ba8cf592f
Pedro Julio G <1102291@est.intec.edu.do>
2025-01-14 10:59:59 -0400

    add authentication function postman

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,18 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-[ApiController]
-[Route("api/[controller]")]
-    public class UsersController(DataContext context) : ControllerBase
+    public class UsersController(DataContext context) : BaseApiController
     {
+        [AllowAnonymous]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -21,7 +17,7 @@ namespace API.Controllers
         return users;
         
         }
-
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,56 @@
 using API.Data;
 using API.Entities;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
+
+// Para JwtBearerDefaults
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+// Para TokenValidationParameters y SymmetricSecurityKey
+using Microsoft.IdentityModel.Tokens;
+
+// Para Encoding.UTF8
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Agregar servicios al contenedor
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt=>
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-}
-);
+});
+
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // Aquí reemplazamos "exception" minúscula por "Exception" mayúscula
+        var tokenKey = builder.Configuration["TokenKey"] ?? throw new Exception("Token not found");
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+        };
+    });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar el pipeline HTTP
+app.UseCors(x => x.AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .WithOrigins("http://localhost:4200/", "https://localhost:4200/"));
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200/", "https://localhost:4200/"));
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
